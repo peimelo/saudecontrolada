8bd1b73fb4ff9a9c3ec303e5c0a01801ed7d8266
Melo <peimelo@gmail.com>
2014-05-30 23:38:54 -0300

    database.yml no capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, 'git@bitbucket.org:peimelo/saudecontrolada.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/env.rb}
+set :linked_files, %w{config/database.yml config/env.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
