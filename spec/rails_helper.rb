ff6b92eab011f45455320d1ac44d1b18e1020f81
Paulo Melo <peimelo@gmail.com>
2015-05-25 20:36:21 -0300

    Mesclando com SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,12 @@
 require 'simplecov'
-SimpleCov.start
+require 'codeclimate-test-reporter'
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
+# SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
