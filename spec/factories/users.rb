99e69e97273d4c8ab2bd70075c4a5f83c3d381cf
Paulo Melo <peimelo@users.noreply.github.com>
2015-05-29 10:52:27 -0300

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,9 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    date_of_birth { rand(1..100).days.from_now.strftime("%d/%m/%Y") }
     sequence(:email) { Faker::Internet.email }
+    gender 'Masculino'
     sequence(:name) { Faker::Name.first_name }
     password 'senha123'
     password_confirmation 'senha123'
