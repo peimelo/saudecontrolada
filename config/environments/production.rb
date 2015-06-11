03c7b5296c098d4c1b1246f25b7ee3b1561dad38
Melo <peimelo@gmail.com>
2015-06-11 21:09:40 +0000

    Alterando formato de email exception para html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,13 @@ Saudecontrolada::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # rotacao do arquivo de log, 5 de 100Mb cada, por exemplo, production.log, production.log.0, production.log.1, ...
-  config.logger = Logger.new(Rails.root.join("log", Rails.env + ".log"), 5, 100*1024*1024)
+  config.logger = Logger.new(Rails.root.join('log', Rails.env + '.log'), 5, 100*1024*1024)
   
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   email: {
     email_prefix: "[ERROR] ",
+    email_format: :html,
     sender_address: %{"notifier" <notifier@example.com>},
     exception_recipients: %w{exceptions@example.com}
   }
