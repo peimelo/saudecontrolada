e2afd3764be3767955aa801ce84b8fb20e79444f
Melo <peimelo@gmail.com>
2015-06-12 13:48:51 -0300

    Adicionando teste de valor

--- spec/models/valor_spec.rb
+++ spec/models/valor_spec.rb
@@ -8,13 +8,35 @@ RSpec.describe Valor do
 
   describe 'validations' do
     it { should validate_presence_of(:exame_id) }
-=begin
-    it do
-      subject { create(:valor, idade_inferior: 15, idade_superior: 25) }
-      should validate_numericality_of(:idade_inferior).is_less_than_or_equal_to(:idade_superior)
+
+    it 'idade_inferior less than or equal to idade_superior' do
+      [20, 21].each do |idade_superior|
+        expect(build(:valor, idade_inferior: 20, idade_superior: idade_superior)).to be_valid
+      end
     end
 
-    it { should validate_numericality_of(:peso).is_greater_than_or_equal_to(3.35).is_less_than_or_equal_to(400) }
-=end
+    it 'idade_superior greater than or equal to idade_inferior' do
+      [19, 20].each do |idade_inferior|
+        expect(build(:valor, idade_superior: 20, idade_inferior: idade_inferior)).to be_valid
+      end
+    end
+
+    it 'valor_inferior less than or equal to valor_superior' do
+      [20, 21].each do |valor_superior|
+        expect(build(:valor, valor_inferior: 20, valor_superior: valor_superior)).to be_valid
+      end
+    end
+
+    it 'valor_superior greater than or equal to valor_inferior' do
+      [19, 20].each do |valor_inferior|
+        expect(build(:valor, valor_superior: 20, valor_inferior: valor_inferior)).to be_valid
+      end
+    end
+
+    it { should validate_numericality_of(:idade_inferior).is_less_than_or_equal_to(199.999) }
+    it { should validate_numericality_of(:idade_superior).is_less_than_or_equal_to(199.999) }
+
+    it { should validate_numericality_of(:valor_inferior).is_less_than_or_equal_to(99999999.99) }
+    it { should validate_numericality_of(:valor_superior).is_less_than_or_equal_to(99999999.99) }
   end
 end
