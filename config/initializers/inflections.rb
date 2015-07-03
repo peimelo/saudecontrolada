0a7cdc999730d205c25f3ee0ee50cf1103d869a7
Paulo Melo <peimelo@gmail.com>
2015-07-03 20:43:32 -0300

    Atualizando arquivos de configuracao para versao 4.2.3

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,10 +1,18 @@
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym 'API'
+# Be sure to restart your server when you modify this file.
 
-  inflect.irregular 'acao', 'acoes'
-  inflect.irregular 'controlador', 'controladores'
-  inflect.irregular 'erro_status', 'erros_status'
-  inflect.irregular 'recurso_categoria', 'recursos_categorias'
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
   inflect.irregular 'referencia', 'referencias'
   inflect.irregular 'valor', 'valores'
 end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
