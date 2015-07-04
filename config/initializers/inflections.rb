de72ef3ef75fc42d509ef29d6eb5fc33463ccbe6
Paulo Melo <peimelo@gmail.com>
2015-07-03 23:54:02 -0300

    Inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,6 +8,10 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
+  inflect.irregular 'acao', 'acoes'
+  inflect.irregular 'controlador', 'controladores'
+  inflect.irregular 'erro_status', 'erros_status'
+  inflect.irregular 'recurso_categoria', 'recursos_categorias'
   inflect.irregular 'referencia', 'referencias'
   inflect.irregular 'valor', 'valores'
 end
