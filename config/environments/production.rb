eb84044bf4e6d2a3e4a2b4066bc0495545b95c0d
Paulo Melo <peimelo@gmail.com>
2015-12-19 17:13:35 -0200

    Retirando rotação de log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # ---------------------------------------------
   # rotacao do arquivo de log, 5 de 100Mb cada, por exemplo, production.log, production.log.0, production.log.1, ...
-  config.logger = Logger.new(Rails.root.join('log', Rails.env + '.log'), 5, 100*1024*1024)
+  # config.logger = Logger.new(Rails.root.join('log', Rails.env + '.log'), 5, 100*1024*1024)
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
