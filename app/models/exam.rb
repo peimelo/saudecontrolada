649efd7ac4c6fcb35848d303ae61969f8bed663f
Paulo Melo <peimelo@gmail.com>
2016-07-10 18:48:43 -0300

    Change names from columns

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -4,7 +4,7 @@ class Exam < ApplicationRecord
   # belongs_to :unit
 
   has_many :exam_result, dependent: :restrict_with_error
-  has_many :result, through: :exame_resultado
+  has_many :result, through: :exam_result
   has_many :valor, dependent: :delete_all
   accepts_nested_attributes_for :valor, allow_destroy: true, reject_if: :all_blank
 
