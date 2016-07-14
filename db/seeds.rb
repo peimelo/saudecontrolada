1dea6662e6aa2fdb90df72e7dfae8197cb296d55
Paulo Melo <peimelo@gmail.com>
2016-07-14 06:50:40 -0300

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'csv'
 def load_file(klass)
   csv_text = File.read(Rails.root.join('db', 'data', "#{klass}.csv"))
   csv = CSV.parse(csv_text, headers: true)
-  p "loading #{klass}..."
+  p "loading #{klass} ..."
   csv.each do |row|
     begin
       register = klass.find_by_id(row['id'])
@@ -16,9 +16,12 @@ def load_file(klass)
       p "error: #{ e } | #{ row.to_hash }"
     end
   end
-  p "done #{klass}"
+  p "... done #{klass}"
 end
 
 load_file(Reference)
 load_file(Unit)
 load_file(Exam)
+
+User.where(gender: 'Feminino').update_all(gender: 'F')
+User.where(gender: 'Masculino').update_all(gender: 'M')
