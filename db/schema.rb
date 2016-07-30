0cc67140f8a449ddb31e40486ae5ccb801dc90b1
Paulo Melo <peimelo@gmail.com>
2016-07-30 16:32:39 -0300

    Factory da data dos Pesos com random para 400 dias

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712230200) do
+ActiveRecord::Schema.define(version: 20160730184937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 20160712230200) do
     t.integer  "user_id",                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["date", "user_id"], name: "index_weights_on_date_and_user_id", unique: true, using: :btree
     t.index ["user_id"], name: "pesos_user_id_idx", using: :btree
   end
 
