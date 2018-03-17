4826d15751d35c60815b5684ec9c73df0c0df8e9
Paulo Melo <peimelo@gmail.com>
2018-03-17 12:05:04 -0300

    Removed duplication code

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ConfirmationsController < ApplicationController
+  include Crud
   skip_before_action :authenticate_with_token!
 
   def create
@@ -12,14 +13,6 @@ class Api::V1::ConfirmationsController < ApplicationController
 
   def update
     user = User.confirm_by_token(params[:token])
-
-    if user.errors.empty?
-      render status: :ok, json: {
-        message: I18n.t('confirmations.edit.message'),
-        title: I18n.t('confirmations.edit.title')
-      }
-    else
-      render json: user.errors, status: :unprocessable_entity
-    end
+    render_user(user, 'confirmations.edit')
   end
 end
--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,29 +1,14 @@
 class Api::V1::PasswordsController < ApplicationController
+  include Crud
   skip_before_action :authenticate_with_token!
 
   def create
     user = User.send_reset_password_instructions(params[:email])
-
-    if user.errors.empty?
-      render status: :ok, json: {
-        message: I18n.t('passwords.create.message'),
-        title: I18n.t('passwords.create.title')
-      }
-    else
-      render json: user.errors, status: :unprocessable_entity
-    end
+    render_user(user, 'passwords.create')
   end
 
   def update
     user = User.reset_password_by_token(params)
-
-    if user.errors.empty?
-      render status: :ok, json: {
-        message: I18n.t('passwords.update.message'),
-        title: I18n.t('passwords.update.title')
-      }
-    else
-      render json: user.errors, status: :unprocessable_entity
-    end
+    render_user(user, 'passwords.update')
   end
 end
