b37315afab2d2bd1d3ee5cba3f61d3052cb8e10f
Paulo Melo <peimelo@gmail.com>
2018-06-19 18:11:19 -0300

    Route using redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-import App from './containers/App';
-import reducer from './reducers';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  reducer,
-  composeEnhancers(applyMiddleware(thunk)),
-);
+import { ConnectedRouter } from 'connected-react-router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import store, { history } from './store'
 
 ReactDOM.render(
   <Provider store={store}>
+    <ConnectedRouter history={history}>
     <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
 );
